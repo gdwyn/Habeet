f0ee48e4d2abbe005aa9bc3b53a0d4d03883a590
Godwin Ebri <104990430+godwinnebri@users.noreply.github.com>
2023-11-19 19:07:51 +0100

    Initial Commit

---
+++ Habeet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Habeet
+//
+//  Created by Godwin IE on 19/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Habeet/HabeetApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabeetApp.swift
+//  Habeet
+//
+//  Created by Godwin IE on 19/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct HabeetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
